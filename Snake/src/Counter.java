6f4c7405bdfc34af25b0b3dfd03c3705f89305e6
Sebastian Menzel <s.menzel@sap.com>
2023-04-06 12:19:45 +0200

    reformat code

--- Snake/src/Counter.java
+++ Snake/src/Counter.java
@@ -1,20 +1,19 @@
-
 public class Counter {
 
-	static int punktzahl = -1;
-	static ZEICHENFENSTER zf2;
-	
-	 public Counter() {
-	
-		zf2 = new ZEICHENFENSTER ("Punktzahl",200,50);
-		zf2.zeichneText("Deine Punktzahl du hund: " + punktzahl, 20, 20);
-	}
-	
-	
-	static public void punktZahlErhöhen() {
-		punktzahl ++; 	
-		zf2.fuelleRechteck(0, 0, 200, 50, "weiss");
-		zf2.zeichneText("Deine Punktzahl du hund: " + punktzahl, 20, 20);
-		
-	}
+  static int punktzahl = -1;
+  static ZEICHENFENSTER zf2;
+
+  public Counter() {
+
+    zf2 = new ZEICHENFENSTER("Punktzahl", 200, 50);
+    zf2.zeichneText("Deine Punktzahl du hund: " + punktzahl, 20, 20);
+  }
+
+
+  static public void punktZahlErhöhen() {
+    punktzahl++;
+    zf2.fuelleRechteck(0, 0, 200, 50, "weiss");
+    zf2.zeichneText("Deine Punktzahl du hund: " + punktzahl, 20, 20);
+
+  }
 }
--- Snake/src/Äpfel.java
+++ Snake/src/Äpfel.java
@@ -2,29 +2,31 @@ import java.util.Random;
 
 public class Äpfel {
 
-	final static int DURCHMESSER = 20;
-	static	int xposition;
-	static int yposition;
-	
-	public Äpfel() {
-	 spawn();
-	}
-	public static void spawn() {
-		
-		Counter.punktZahlErhöhen();
-		Random zufally = new Random();
-		int zufallsZahly = zufally.nextInt(Spielfeld.höhe - DURCHMESSER);
-		yposition = zufallsZahly;
-		Random zufallx = new Random();
-		int zufallsZahlx = zufallx.nextInt(Spielfeld.breite - DURCHMESSER+ 15);
-		xposition = zufallsZahlx;
-		Spielfeld.zf.fuelleKreis(zufallsZahlx, zufallsZahly, 10, "rot");
-	}
-	public static void gegessenWerden() {
-		
-		Spielfeld.zf.fuelleKreis(xposition, yposition, DURCHMESSER, "weiss");
-		
-		spawn();
-		
-	}
+  final static int DURCHMESSER = 20;
+  static int xposition;
+  static int yposition;
+
+  public Äpfel() {
+    spawn();
+  }
+
+  public static void spawn() {
+
+    Counter.punktZahlErhöhen();
+    Random zufally = new Random();
+    int zufallsZahly = zufally.nextInt(Spielfeld.höhe - DURCHMESSER);
+    yposition = zufallsZahly;
+    Random zufallx = new Random();
+    int zufallsZahlx = zufallx.nextInt(Spielfeld.breite - DURCHMESSER + 15);
+    xposition = zufallsZahlx;
+    Spielfeld.zf.fuelleKreis(zufallsZahlx, zufallsZahly, 10, "rot");
+  }
+
+  public static void gegessenWerden() {
+
+    Spielfeld.zf.fuelleKreis(xposition, yposition, DURCHMESSER, "weiss");
+
+    spawn();
+
+  }
 }
